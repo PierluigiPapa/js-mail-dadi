109efdfd18e0bcb40dac649fea8836da137f2a72
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-10 15:31:56 +0100

    feat: add const list js

---
+++ Esercizio_Mail/main.js
@@ -0,0 +1,8 @@
+//Costante per creare la lista delle email che possono accedere
+const listaEmailUtenti = ['pierluigipapa9@gmail.com', 'papaeliana90@gmail.com', 'serenapapa99.sp@gmail.com', 'papafrancesco1259@gmail.com', 'alidaannasimone@gmail.com'];
+
+//Variabile per stabilire lo stato iniziale
+let accessoConsentito = false;
+
+//Costante per prompt per inserire l'email
+const emailUtente = prompt ('Inserisci la tua mail?');
