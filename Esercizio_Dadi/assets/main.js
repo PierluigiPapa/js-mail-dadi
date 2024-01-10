30d68d776e771cbb0cd239c9689ff51eb988cfb2
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-10 17:01:27 +0100

    feat: add button html and random pc and player

--- Esercizio_Dadi/assets/main.js
+++ Esercizio_Dadi/assets/main.js
@@ -1,2 +1,17 @@
 //Costante per genere il numero, cliccando sul bottone
 const btnRandom = document.querySelector('button');
+
+//Aggiungere evento al click al bottone per generare i numeri del pc e del giocatore
+btnRandom.addEventListener('click',
+function () {
+
+    //Dichiarare il numero del pc (1,2,3,4,5,6)
+    const numeroComputer = Math.floor(Math.random() * 6) + 1;
+    console.log('Random Computer:', numeroComputer);
+    
+     //Dichiarare il numero del giocatore (1,2,3,4,5,6)
+    const numeroGiocatore = Math.floor(Math.random() * 6) + 1;
+    console.log('Random Giocatore:', numeroGiocatore);
+
+    
+})
